3a128adee6cbf0c929a066491b61863aa2094ae7
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-06-21 21:58:09 +0300

    feat: completed header page 😋 👩🏻‍🎨

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,54 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import Categories from "../components/Home/Categories";
-import LatestArticles from "../components/Home/LatestArticles";
-import Slider from "../components/Shared/Slider";
-import { fetchAllPerson } from "../store/personSlice";
+import React from "react";
+import ok from "../assets/ok.png";
+import time from "../assets/time.svg";
 
-function Home(props) {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchAllPerson());
-  }, []);
-
+function Home() {
   return (
     <div>
-      <Slider />
-      <div className="container flex flex-col mx-auto my-10 md:flex-row">
-        <LatestArticles />
-        <Categories />
+      <div className="container flex mx-auto mt-12">
+        <img src={ok} alt="" className="w-96 h-96" />
+        <div className="py-3 pl-12">
+          <h1 className="text-6xl font-bold text-blue-700">
+            Zaman <br />
+            <span>Çizelgesi</span>
+          </h1>
+          <p className="mt-5 text-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+            ea ad laborum in praesentium, cum quae eligendi architecto,
+            consectetur perferendis minus accusamus tempore delectus, ut autem
+            aliquid odio dolorem porro? Voluptatibus illo dolorem distinctio
+            iusto explicabo tempore sunt consequatur rerum aperiam, iste, fuga
+            saepe laboriosam laborum incidunt accusantium aspernatur ipsum
+            consequuntur eos! Vel placeat velit, repudiandae recusandae
+            voluptates ea doloribus. Facere possimus pariatur omnis nobis
+            cupiditate totam laborum alias doloribus adipisci quidem sunt sed
+            quae eligendi maxime architecto autem rerum, numquam impedit eaque?
+            Quo corrupti distinctio illo dolores, deleniti rerum. Excepturi
+            tempora at, veniam quidem ex cupiditate ducimus harum facilis
+            itaque.
+          </p>
+        </div>
+      </div>
+      <div className="container flex items-center mx-auto">
+        <div className="py-3 pr-12">
+          <h1 className="text-6xl font-bold text-blue-700">Kronoloji</h1>
+          <p className="mt-5 text-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+            ea ad laborum in praesentium, cum quae eligendi architecto,
+            consectetur perferendis minus accusamus tempore delectus, ut autem
+            aliquid odio dolorem porro? Voluptatibus illo dolorem distinctio
+            iusto explicabo tempore sunt consequatur rerum aperiam, iste, fuga
+            saepe laboriosam laborum incidunt accusantium aspernatur ipsum
+            consequuntur eos! Vel placeat velit, repudiandae recusandae
+            voluptates ea doloribus. Facere possimus pariatur omnis nobis
+            cupiditate totam laborum alias doloribus adipisci quidem sunt sed
+            quae eligendi maxime architecto autem rerum, numquam impedit eaque?
+            Quo corrupti distinctio illo dolores, deleniti rerum. Excepturi
+            tempora at, veniam quidem ex cupiditate ducimus harum facilis
+            itaque.
+          </p>
+        </div>
+        <img src={time} alt="" className="w-[500px] h-[500px] object-contain" />
       </div>
     </div>
   );
